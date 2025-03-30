bf621e4f215dbe209a535a3aab0d335afbc1e8fd
Sarika Meera <sarikameeralakhani@gmail.com>
2025-03-30 16:40:55 -0500

    ValueErrors are raised for invalid inputs with a message indicating why.

--- my_code.py
+++ my_code.py
@@ -1,6 +1,8 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
+  if not phone_num_to_fix.isdigit():
+    raise ValueError("Format must only be digits and no other characters (ex: 6509466488)")
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
@@ -17,5 +19,7 @@ def test_fix_phone_num():
   assert fix_phone_num("3216543333") == "(321) 654 3333"
 
 def test_diff_format_nums():
-  assert fix_phone_num("555-442-98761") == "(555) 442 98761"
-  assert fix_phone_num("(321) 654 3333") == "(321) 654 3333"
+  with pytest.raises(ValueError) as phone_num:
+    fix_phone_num("555-442-9876")
+    fix_phone_num("(321) 654 3333")
+  assert str(phone_num.value) == "Format must only be digits and no other characters (ex: 6509466488)"
